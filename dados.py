14ff863121b2a97e2b69fca2f67d7c55c95b5b6f
Vitor Matos <vitormatos@bemol.com.br>
2024-04-23 13:57:46 -0400

    adicionei alterações nos comentários

--- dados.py
+++ dados.py
@@ -31,12 +31,11 @@ volume_vendas_por_categoria = merged_df.groupby('product_category_name')['order_
 volume_vendas_por_categoria.columns = ['product_category_name', 'volume_vendas']
 volume_vendas_por_categoria = volume_vendas_por_categoria.sort_values(by='volume_vendas', ascending=False)
 
-# Imprimir volume de vendas por categoria de produto
 print("Volume de Vendas por Categoria de Produto:")
 print(volume_vendas_por_categoria)
 print("\n\n")
 
-# Plotar gráfico de barras do volume de vendas por categoria de produto
+# Gráfico de barras do volume de vendas por categoria de produto
 plt.figure(figsize=(10, 6))
 plt.bar(volume_vendas_por_categoria['product_category_name'], volume_vendas_por_categoria['volume_vendas'], color='skyblue')
 plt.xlabel('Categoria de Produto')
@@ -49,32 +48,32 @@ plt.show()
 
 print('2. Análise de Logística')
 print('\na. Prazos de Entrega: Calcular o tempo médio de entrega e identificar os fatores que influenciam atrasos nas entregas.')
-# Passo 1: Calcular o tempo de entrega para cada pedido.
-# Converter as colunas de data para datetime
+# Passo 1: Calculando o tempo de entrega para cada pedido.
+# Convertendo as colunas de data para datetime
 df_pedidos['order_purchase_timestamp'] = pd.to_datetime(df_pedidos['order_purchase_timestamp'])
 df_pedidos['order_delivered_customer_date'] = pd.to_datetime(df_pedidos['order_delivered_customer_date'], errors='coerce')
 
-# Calcular o tempo de entrega para cada pedido
+# Calculando o tempo de entrega para cada pedido
 df_pedidos['tempo_entrega'] = df_pedidos['order_delivered_customer_date'] - df_pedidos['order_purchase_timestamp']
 
-# Passo 2: Calcular o tempo médio de entrega.
-# Calcular o tempo médio de entrega, excluindo valores "data indisponível"
+# Passo 2: Calculando o tempo médio de entrega.
+# Calculando o tempo médio de entrega, excluindo valores "data indisponível"
 tempo_medio_entrega = df_pedidos[df_pedidos['order_delivered_customer_date'].notnull()]['tempo_entrega'].mean()
 print("Tempo Médio de Entrega: ", tempo_medio_entrega)
 print("\n\n")
 
-# Calcular o tempo de trânsito de cada pedido
+# Calculando o tempo de trânsito de cada pedido
 df_pedidos['order_approved_at'] = pd.to_datetime(df_pedidos['order_approved_at'], errors='coerce')
 df_pedidos['order_delivered_carrier_date'] = pd.to_datetime(df_pedidos['order_delivered_carrier_date'], errors='coerce')
 df_pedidos['order_delivered_customer_date'] = pd.to_datetime(df_pedidos['order_delivered_customer_date'], errors='coerce')
 
-# Filtrar pedidos com todas as datas disponíveis
+# Filtrando pedidos com todas as datas disponíveis
 df_pedidos_validos = df_pedidos.dropna(subset=['order_approved_at', 'order_delivered_carrier_date', 'order_delivered_customer_date'])
 
-# Calcular o tempo de trânsito
+# Calculando o tempo de trânsito
 df_pedidos_validos['tempo_transito'] = df_pedidos_validos['order_delivered_customer_date'] - df_pedidos_validos['order_approved_at']
 
-# Exibir estatísticas do tempo de trânsito
+# Exibindo estatísticas do tempo de trânsito
 print("Estatísticas do Tempo de Trânsito:")
 print(df_pedidos_validos['tempo_transito'].describe())
 print("\n\n\n")
@@ -82,26 +81,25 @@ print("\n\n\n")
 
 print('3. Análise de Satisfação do Cliente')
 print('\na. Avaliações de Produtos: Analisar a distribuição das avaliações dos produtos e identificar os produtos com as melhores e piores avaliações')
-# Passo 1: Fundir os DataFrames df_avaliacoes_pedido e df_itens_pedido usando a coluna 'order_id' como chave de junção
+# Passo 1: Fundindo os DataFrames df_avaliacoes_pedido e df_itens_pedido usando a coluna 'order_id' como chave de junção
 avaliacoes_itens_fundidos = pd.merge(df_avaliacoes_pedido, df_itens_pedido, on='order_id', how='inner')
 
 # Passo 2: Calcular a média das avaliações de cada produto
 media_avaliacoes_produto = avaliacoes_itens_fundidos.groupby('product_id')['review_score'].mean().reset_index()
 
-# Passo 3: Classificar os produtos com base na média das avaliações
+# Passo 3: Classificando os produtos com base na média das avaliações
 produtos_classificados = media_avaliacoes_produto.sort_values(by='review_score', ascending=False)
 
-# Imprimir estatísticas das avaliações dos produtos
 print("Estatísticas das Avaliações dos Produtos:")
 print(produtos_classificados['review_score'].describe())
 
-# Imprimir os produtos com as melhores e piores avaliações
 print("\nProdutos com as Melhores Avaliações:")
 print(produtos_classificados.head(10))  # Os 10 produtos com as melhores avaliações
 
 print("\nProdutos com as Piores Avaliações:")
 print(produtos_classificados.tail(10))  # Os 10 produtos com as piores avaliações
-# Plotar gráfico de barras das avaliações médias dos produtos
+
+# Gráfico de barras das avaliações médias dos produtos
 plt.figure(figsize=(10, 6))
 plt.bar(produtos_classificados['product_id'].head(10), produtos_classificados['review_score'].head(10), color='lightgreen')
 plt.xlabel('ID do Produto')
@@ -113,7 +111,8 @@ plt.show()
 
 print("\nProdutos com as Piores Avaliações:")
 print(produtos_classificados.tail(10))  # Os 10 produtos com as piores avaliações
-# Plotar gráfico de barras das avaliações médias dos produtos
+
+# Gráfico de barras das avaliações médias dos produtos
 plt.figure(figsize=(10, 6))
 plt.bar(produtos_classificados['product_id'].tail(10), produtos_classificados['review_score'].tail(10), color='lightcoral')
 plt.xlabel('ID do Produto')
@@ -127,28 +126,27 @@ print("\n\n\n")
 
 print('4. Análise Financeira')
 print('\na. Análise de Lucratividade por Categoria: Calcular a lucratividade de diferentes categorias de produtos, levando em conta o custo dos produtos e o preço de venda.')
-# Calcular o custo total de cada categoria de produto
+# Calculando o custo total de cada categoria de produto
 merged_df = pd.merge(df_itens_pedido, df_produtos, on='product_id', how='inner')
 custo_por_categoria = merged_df.groupby('product_category_name')['price'].sum().reset_index()
 custo_por_categoria.columns = ['product_category_name', 'custo_total']
 
-# Calcular o preço total de venda de cada categoria de produto
+# Calculando o preço total de venda de cada categoria de produto
 preco_por_categoria = merged_df.groupby('product_category_name')['freight_value'].sum().reset_index()
 preco_por_categoria.columns = ['product_category_name', 'preco_total']
 
-# Calcular a lucratividade de cada categoria de produto
+# Calculando a lucratividade de cada categoria de produto
 lucratividade_por_categoria = pd.merge(custo_por_categoria, preco_por_categoria, on='product_category_name', how='inner')
 lucratividade_por_categoria['lucro'] = lucratividade_por_categoria['preco_total'] - lucratividade_por_categoria['custo_total']
 
-# Ordenar por lucratividade da maior para a menor
+# Ordenando por lucratividade da maior para a menor
 lucratividade_por_categoria = lucratividade_por_categoria.sort_values(by='lucro', ascending=False)
 
-# Imprimir lucratividade por categoria de produto
 print("\nLucratividade por Categoria de Produto (da maior para a menor):")
 print(lucratividade_por_categoria)
 print("\n\n\n")
 
-# Plotar gráfico de barras da lucratividade por categoria de produto
+# Gráfico de barras da lucratividade por categoria de produto
 plt.figure(figsize=(10, 6))
 plt.bar(lucratividade_por_categoria['product_category_name'], lucratividade_por_categoria['lucro'], color='orange')
 plt.xlabel('Categoria de Produto')
@@ -161,29 +159,28 @@ plt.show()
 
 print(' 5. Análise de Marketing')
 print('\na. Análise de Conversão de Vendas: Estudar a taxa de conversão de vendas com base em diferentes fontes de tráfego (orgânico, pago, social, etc.).')
-# Passo 1: Filtrar transações concluídas
+# Passo 1: Filtrando transações concluídas
 transacoes_concluidas = df_pedidos[df_pedidos['order_status'] == 'delivered']
 
-# Passo 2: Adicionar a coluna 'payment_type' ao DataFrame `transacoes_concluidas`
+# Passo 2: Adicionando a coluna 'payment_type' ao DataFrame `transacoes_concluidas`
 transacoes_concluidas = pd.merge(transacoes_concluidas, df_pagamentos_pedido[['order_id', 'payment_type']], on='order_id', how='inner')
 
-# Passo 3: Agrupar as transações concluídas por tipo de pagamento
+# Passo 3: Agrupando as transações concluídas por tipo de pagamento
 grupo_transacoes_concluidas = transacoes_concluidas.groupby('payment_type')
 
-# Passo 4: Calcular o número de transações concluídas para cada tipo de pagamento
+# Passo 4: Calculando o número de transações concluídas para cada tipo de pagamento
 total_transacoes_concluidas = grupo_transacoes_concluidas.size().reset_index(name='transacoes_concluidas')
 
-# Passo 5: Calcular o número total de transações concluídas
+# Passo 5: Calculando o número total de transações concluídas
 total_transacoes = total_transacoes_concluidas['transacoes_concluidas'].sum()
 
-# Passo 6: Calcular a taxa de conversão de vendas para cada tipo de pagamento
+# Passo 6: Calculando a taxa de conversão de vendas para cada tipo de pagamento
 total_transacoes_concluidas['taxa_conversao'] = (total_transacoes_concluidas['transacoes_concluidas'] / total_transacoes) * 100
 
-# Exibir os resultados
 print("\nTaxa de Conversão de Vendas por Tipo de Pagamento:")
 print(total_transacoes_concluidas[['payment_type', 'taxa_conversao']])
 
-## Plotar gráfico de pizza da taxa de conversão de vendas por tipo de pagamento
+## Gráfico de pizza da taxa de conversão de vendas por tipo de pagamento
 plt.figure(figsize=(8, 8))
 plt.pie(total_transacoes_concluidas['taxa_conversao'], labels=total_transacoes_concluidas['payment_type'], autopct='%1.1f%%')
 plt.title('Taxa de Conversão de Vendas por Tipo de Pagamento')
